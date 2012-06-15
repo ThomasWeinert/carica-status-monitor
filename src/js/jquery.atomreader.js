013bb3a591b9e44d81313e20bf1e6d860e0fe553
Thomas Weinert <thomas@weinert.info>
2012-06-16 00:01:11 +0200

    Added basic feed fetching and status handling

--- src/js/jquery.atomreader.js
+++ src/js/jquery.atomreader.js
@@ -2,12 +2,54 @@
 
   var AtomReader = {
 
+    node : null,
+
+    options : {
+      url : '',
+      mode : 'all',
+      max : 5,
+      interval : 60
+    },
+
+    xmlns : {
+      'atom' : 'http://www.w3.org/2005/Atom',
+      'media' : 'http://search.yahoo.com/mrss/',
+      'carica' : 'http://www.a-basketful-of-papays.net/ns/status-monitor'
+    },
+
+    setUp : function(node, options) {
+      this.node = node;
+      this.options = $.extend($.extend(this.options, options), node.data());
+      this.fetch();
+      window.setInterval($.proxy(this.fetch, this), 1000 * this.options.interval);
+    },
+
+    fetch : function() {
+      console.log(this.options);
+      if (this.options.url != '') {
+        this.node.find('.status').removeClass('error').addClass('loading').slideDown();
+        $.get(this.options.url)
+          .success($.proxy(this.ajaxSuccess, this))
+          .error($.proxy(this.ajaxError, this));
+      }
+    },
+
+    ajaxSuccess : function(data) {
+      this.node.find('.status').slideUp();
+    },
+
+    ajaxError : function(data) {
+      this.node.find('.status').removeClass('loading').addClass('error').text(
+        data.status + ' ' + data.statusText
+      );
+    }
   };
 
   $.fn.AtomReader = function(options) {
     return this.each(
       function() {
-
+        var feed = $.extend(true, {}, AtomReader);
+        feed.setUp($(this), options);
       }
     );
   };
