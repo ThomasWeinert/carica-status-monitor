15b608f8c49dbe9bc7cb4da0d86d89a3aeeb24c2
Thomas Weinert <thomas@weinert.info>
2012-06-17 20:30:28 +0200

    Documented parameter and return value in interface

--- src/library/Filter.php
+++ src/library/Filter.php
@@ -4,6 +4,10 @@ namespace Carica\StatusMonitor\Library {
 
   interface Filter {
 
+    /**
+     * @param \DOMDocument $dom
+     * @return \DOMDocument
+     */
     public function filter(\DOMDocument $dom);
 
   }
