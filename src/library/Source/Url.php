33b755f727992e40ba0dc9f9232250b3f8a4e588
Thomas Weinert <thomas@weinert.info>
2012-06-18 19:11:03 +0200

    Added test domain mapping in url data source object

--- src/library/Source/Url.php
+++ src/library/Source/Url.php
@@ -20,13 +20,26 @@ namespace Carica\StatusMonitor\Library\Source {
      * @return \DOMDocument
      */
     public function read() {
-      $xml = file_get_contents($this->_url);
+      $xml = file_get_contents($this->getUrl());
       if (!empty($xml)) {
         $dom = new \DOMDocument();
         $dom->loadXml($xml);
         return $dom;
       }
       return NULL;
+    }
+    
+    private function getUrl() {
+      if (!preg_match('(^https?://)', $this->_url)) {
+        throw new UnexpectedValueException('Source url is invalid.');
+      }
+      $exampleUrl = 'http://example.tld/';
+      if (0 === strpos($this->_url, $exampleUrl)) {
+        return realpath(
+          __DIR__.'/../../../tests/files/'.substr($this->_url, strlen($exampleUrl))
+        );
+      }
+      return $this->_url;
     }
   }
 }
