ae723d8a5d14ed5addb52a07f6ff83d02c31713b
Thomas Weinert <thomas@weinert.info>
2012-06-18 19:13:17 +0200

    Use the library classes to fetch the twitter feed

--- src/feeds/twitter.php
+++ src/feeds/twitter.php
@@ -4,7 +4,17 @@ if (empty($_GET['q'])) {
   header('Status: 400 Bad Request', TRUE, 400);
   exit;
 }
-header('Content-Type: text/xml');
-readfile(
-  'http://search.twitter.com/search.atom?q='.urlencode($_GET['q'])
+
+use Carica\StatusMonitor\Library as Library;
+
+include_once(__DIR__.'/../library/Autoloader.php');
+Library\Autoloader::register();
+
+$feed = new Library\Feed(
+  new Library\Source\Url(
+    'http://search.twitter.com/search.atom?q='.urlencode($_GET['q'])
+  )
 );
+
+header('Content-Type: text/xml');
+echo (string)$feed;
