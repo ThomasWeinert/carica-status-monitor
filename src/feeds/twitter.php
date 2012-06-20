610b37404a88c5dc9c3cc2e2715edd89f2de15d8
Thomas Weinert <thomas@weinert.info>
2012-06-20 10:59:59 +0200

    Fetch twitter user timelines

--- src/feeds/twitter.php
+++ src/feeds/twitter.php
@@ -1,14 +1,21 @@
 <?php
 /**
-* Do a Twitter search and return the result.
+* Twitter provides nice Atom feeds.
 *
-* Twitter provides a nice Atom feed.
+* Usage:
+*
+* Do a Twitter search:.
+*   twitter.php?q=%23papayaCMS
+*
+* Fetch the User timeline:.
+*   twitter.php?user=papayaCMS
+*
 *
 * @license http://www.opensource.org/licenses/mit-license.php The MIT License
 * @copyright 2012 Thomas Weinert <thomas@weinert.info>
 */
 
-if (empty($_GET['q'])) {
+if (empty($_GET['q']) && empty($_GET['user'])) {
   header('Status: 400 Bad Request', TRUE, 400);
   exit;
 }
@@ -18,9 +25,15 @@ use Carica\StatusMonitor\Library as Library;
 include_once(__DIR__.'/../library/Autoloader.php');
 Library\Autoloader::register();
 
+if (!empty($_GET['user'])) {
+  $url = 'https://twitter.com/statuses/user_timeline/'.urlencode($_GET['user']).'.atom';
+} else {
+  $url = 'http://search.twitter.com/search.atom?q='.urlencode($_GET['q']);
+}
+
 $feed = new Library\Feed(
   new Library\Source\Url(
-    'http://search.twitter.com/search.atom?q='.urlencode($_GET['q'])
+    $url
   )
 );
 
