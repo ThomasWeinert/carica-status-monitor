bc5dff3224b49c41617a42a77d6f4a5401c7d833
Thomas Weinert <thomas@weinert.info>
2012-07-02 21:42:52 +0200

    Tested: Caches.php

--- tests/library/CachesTest.php
+++ tests/library/CachesTest.php
@@ -53,5 +53,56 @@ namespace Carica\StatusMonitor\Library {
       $caches['bucket'] = $service;
       $this->assertSame($service, $caches['bucket']);
     }
+
+    /**
+     * @covers \Carica\StatusMonitor\Library\Caches::get
+     * @covers \Carica\StatusMonitor\Library\Caches::offsetGet
+     */
+    public function testOffsetGetFromConfiguration() {
+      $profile = $this->getMock(
+        '\\Carica\\StatusMonitor\\Library\\Cache\\Configuration'
+      );
+      $profile
+        ->expects($this->atLeastOnce())
+        ->method('get')
+        ->withAnyParameters()
+        ->will(
+          $this->returnValueMap(
+            array(
+              array('SERVICE', 'file',  'file'),
+              array('PATH', '', '/path/')
+            )
+          )
+        );
+      $caches = new Caches();
+      $caches->register('bucket', $profile);
+      $this->assertInstanceOf(
+        '\\Carica\\StatusMonitor\\Library\\Cache\\Service\\File',
+        $caches['bucket']
+      );
+    }
+
+    /**
+     * @covers \Carica\StatusMonitor\Library\Caches::get
+     * @covers \Carica\StatusMonitor\Library\Caches::offsetGet
+     */
+    public function testOffsetGetExpectingException() {
+      $caches = new Caches();
+      $this->setExpectedException('\\InvalidArgumentException', 'Cache bucket not found.');
+      $dummy = $caches['bucket'];
+    }
+
+    /**
+     * @covers \Carica\StatusMonitor\Library\Caches::offsetUnset
+     */
+    public function testOffsetUnset() {
+      $service = $this->getMock(
+        '\Carica\StatusMonitor\Library\Cache\Service'
+      );
+      $caches = new Caches();
+      $caches['bucket'] = $service;
+      unset($caches['bucket']);
+      $this->assertFalse(isset($caches['bucket']));
+    }
   }
 }
