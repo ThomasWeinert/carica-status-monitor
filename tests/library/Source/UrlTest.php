52e41e9dfee18e4a1b4a23a94d34ab5637d6f87e
Thomas Weinert <thomas@weinert.info>
2012-07-09 09:37:33 +0200

    Tested \Carica\StatusMonitor\Library\Source\Url

---
+++ tests/library/Source/UrlTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Carica\StatusMonitor\Library\Source {
+
+  require_once(__DIR__.'/../TestCase.php');
+
+  use \Carica\StatusMonitor\Library as Library;
+
+  class UrlTest extends Library\TestCase {
+
+    /**
+     * @covers Carica\StatusMonitor\Library\Source\Url::__construct
+     */
+    public function testConstructorWithAllParameters() {
+      $source = new Url('http://example.tld/success.xml', 23);
+      $this->assertAttributeEquals(
+        'http://example.tld/success.xml', '_url', $source
+      );
+      $this->assertAttributeEquals(
+        23, '_timeout', $source
+      );
+    }
+
+    /**
+     * @covers Carica\StatusMonitor\Library\Source\Url::read
+     */
+    public function testReadExpectingDom() {
+      $source = new Url('http://example.tld/sample-feed.xml');
+      $this->assertInstanceOf(
+        '\DOMDocument', $source->read()
+      );
+    }
+
+    /**
+     * @covers Carica\StatusMonitor\Library\Source\Url::read
+     */
+    public function testReadExpectingNull() {
+      $source = new Url('http://example.tld/INVALID.xml');
+      $this->assertNull(
+        $source->read()
+      );
+    }
+  }
+}
